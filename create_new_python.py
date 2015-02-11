0a3966622b11e9da5c99fed51e17e584f1559ad2
yurket <yurket2006@gmail.com>
2015-02-11 11:18:31 +0300

    added C source template

--- create_new_python.py
+++ create_new_python.py
@@ -4,10 +4,9 @@ from __future__ import print_function
 
 import argparse
 import os
-import sys
 
 
-TEMPLATE = \
+PYTHON_TEMPLATE = \
 u"""#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -24,18 +23,47 @@ if __name__ == '__main__':
 
 """
 
+C_TEMPLATE = \
+u"""#include <stdio.h>
+
+
+int main()
+{
+
+    return 0;
+}
+
+"""
+
+class COLORS:
+    NOCOLOR = '\033[0m'
+    RED = '\033[91m'
+
+
 def main():
     DESCR = 'program creates ready-to-use template for average python script.'
     parser = argparse.ArgumentParser(description=DESCR)
-    parser.add_argument('filename', type=str, help='filelame of new python source file')
+    parser.add_argument('src_type', type=str, help='source file type [python, c]')
+    parser.add_argument('filename', type=str, help='filelame of new source file')
     args = parser.parse_args()
 
     filename = args.filename
-    name, ext = os.path.splitext(filename)
-    if not ext:
-        filename += '.py'
-    with open(filename, 'wb') as f:
-        f.write(TEMPLATE)
+    src_type = args.src_type.lower()
+    if src_type == 'python':
+        name, ext = os.path.splitext(filename)
+        if not ext:
+            filename += '.py'
+        with open(filename, 'wb') as f:
+            f.write(PYTHON_TEMPLATE)
+    elif src_type == 'c':
+        name, ext = os.path.splitext(filename)
+        if not ext:
+            filename += '.cc'
+        with open(filename, 'wb') as f:
+            f.write(C_TEMPLATE)
+    else:
+        print("Error: Wrong %ssrc_type%s! Specify one of the supported: 'c' or 'python'" %
+              (COLORS.RED, COLORS.NOCOLOR))
 
 
 if __name__ == '__main__':
