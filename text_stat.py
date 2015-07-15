6ba5d7de709d0894d1b8b182db3e4824d88eeaca
yurket <yurket2006@yandex.ru>
2015-07-15 23:16:13 +0300

    added frequency statistics help printings

--- text_stat.py
+++ text_stat.py
@@ -11,6 +11,20 @@ import string
 from collections import Counter, OrderedDict
 
 g_SEP = '-'*100
+g_TOP_LETTERS  = 'E T A O I N S H R D L U'
+
+g_TOP_WORDS = {
+      0: ''
+    , 1: ''
+    , 2: 'of to in it is be as at so we he by or on do'
+    , 3: 'the and for are but not you all'
+    , 4: 'that with from this'
+    }
+
+g_LETTER_FREQ_STAT = {
+    (2, False): 'Top digraphs: th er on an re he in ed nd ha at en es of \n'
+    , (2, True): 'Top doubles: ss ee tt ff ll mm oo \n'
+}
 
 
 def print_letters_stats(data):
@@ -26,35 +40,36 @@ def print_letters_stats(data):
     print()
     keys = sorted(ctr.keys())
     printable_keys = (' '.join(keys)).replace('\n', '\\n')
-    print('Whole alphabet is: %s' % printable_keys)
-    print('Lengh: %d' % len(keys))
+    print('Whole alphabet is: %s     (Lengh: %d)' % (printable_keys, len(keys)))
     print(g_SEP)
 
     without_whitespaces = [k for k in keys if k not in string.whitespace]
-    print('Without whitespaces: %s' % ' '.join(without_whitespaces))
-    print('Lengh: %d' % len(without_whitespaces))
+    print('Without whitespaces: %s     (Lengh: %d)' %
+          (' '.join(without_whitespaces), len(without_whitespaces)))
     print(g_SEP)
 
     without_punct = [k for k in without_whitespaces if k not in string.punctuation]
-    print('Without punctuation: %s' % ' '.join(without_punct))
-    print('Lengh: %d' % len(without_punct))
+    print('Without punctuation: %s     (Length: %d)' %
+          (' '.join(without_punct), len(without_punct)))
     print(g_SEP)
 
     # print percents
     print('\n\t\tLETTERS DISTRIBUTION: \n')
+    print('Most frequent letters: ' + g_TOP_LETTERS + '\n')
+
     for char, count in ctr.most_common():
         percent = (float(count)/data_len)*100
 
         if char == '\n':
             char = '\\n'
         print("{0}: {1:.2f}% ({2} times)".format(char, percent, count))
-
     return ctr.most_common()
 
 
 def print_words_stats(data):
     print(g_SEP)
     print('\n\t\tWORDS STATISTICS: \n')
+    print()
 
     words = data.split(' ')
     if len(words) < 3:
@@ -64,7 +79,14 @@ def print_words_stats(data):
     most_common = Counter(words).most_common()
     most_common = sorted(most_common, key=lambda x: len(x[0]))
     most_common = [ (k,v) for k,v in most_common if v > 1]
+
+    prev_word_len = len(most_common[0])
     for word, count in most_common:
+        if len(word) != prev_word_len:
+            print('-'*50)
+            print('\nTOP %d-L words:  %s' % (len(word), g_TOP_WORDS[len(word)]))
+            prev_word_len = len(word)
+
         percent = (float(count)/len(words))*100
         print("{0}: {1:.2f}% ({2} times)".format(word, percent, count))
 
@@ -79,6 +101,7 @@ def print_letters_stats2(data, letters_count, count_same_letters=False):
     print(g_SEP)
     print_str = '(repeated letters)' if count_same_letters else ''
     print('\n\t\t%d-GRAPHS %s STATISTICS: \n' % (letters_count, print_str))
+    print(g_LETTER_FREQ_STAT[(letters_count, count_same_letters)])
 
     letters_map = {}
     for i in xrange(len(data)-letters_count+1):
@@ -99,6 +122,7 @@ def print_letters_stats2(data, letters_count, count_same_letters=False):
 
     for k,v in sorted_letters_map.items()[:10]:
         print(k,v)
+
 
 
 def main():
@@ -123,7 +147,7 @@ def main():
     print_words_stats(data)
 
     print_letters_stats2(data, 2)
-    print_letters_stats2(data, 3)
+    # print_letters_stats2(data, 3)
 
     COUNT_SAME = True
     print_letters_stats2(data, 2, COUNT_SAME)
