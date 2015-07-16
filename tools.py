abbb3103aaaef24de99d4e4669f5dd8fdd37cd30
yurket <yurket2006@yandex.ru>
2015-07-16 22:25:26 +0300

    1) changed indentation to 4 spaces, 2) added xor_ functions

--- tools.py
+++ tools.py
@@ -5,22 +5,38 @@ from __future__ import print_function
 
 
 def string_to_bytes(s):
-  return [ord(x) for x in s]
+    return [ord(x) for x in s]
 
 def bytes_to_string(b):
-  return ''.join(chr(x) for x in b)
+    return ''.join(chr(x) for x in b)
 
 def string_to_hex(s):
-  return s.encode('hex')
+    return s.encode('hex')
 
 def hex_to_string(s):
-  return s.decode('hex')
+    return s.decode('hex')
 
 def bytes_to_hex(b):
-  return hex_to_string(bytes_to_string(b))
+    return hex_to_string(bytes_to_string(b))
 
 def hex_to_bytes(s):
-  return string_to_bytes(hex_to_string(s))
+    return string_to_bytes(hex_to_string(s))
+
+
+
+def xor_bytes(a, b):
+    if len(a) != len(b):
+        print('[!] strings of different length!')
+    if len(a) > len(b):
+        return "".join([chr(x^y) for (x,y) in zip(a[:len(b)], b)]).encode('hex')
+    else:
+        return "".join([chr(x^y) for (x,y) in zip(a, b[:len(a)])]).encode('hex')
+
+def xor_strings(a, b):
+    return bytexor(string_to_bytes(a), string_to_bytes(b))
+
+def xor_hexs(a, b):
+    return bytexor(hex_to_bytes(a), hex_to_bytes(b))
 
 
 def main():
