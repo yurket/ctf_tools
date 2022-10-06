88f5e62a8345da693d31305a93994400dc5317c8
yurket <yurket2006@yandex.ru>
2022-10-06 12:51:41 -0700

    Add creation of cpp project with simple CMakeLists.txt; format with Black

--- create_new_src.py
+++ create_new_src.py
@@ -6,8 +6,7 @@ import os
 import stat
 
 
-PYTHON_TEMPLATE = \
-u"""#!/usr/bin/env python3
+PYTHON_TEMPLATE = """#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from typing import List
@@ -22,8 +21,7 @@ if __name__ == '__main__':
 
 """
 
-RUBY_TEMPLATE = \
-u"""#!/usr/bin/env ruby
+RUBY_TEMPLATE = """#!/usr/bin/env ruby
 
 
 def main()
@@ -38,8 +36,7 @@ end
 
 """
 
-C_TEMPLATE = \
-u"""#include <stdio.h>
+C_TEMPLATE = """#include <stdio.h>
 #include <string.h>
 
 #include <errno.h>
@@ -55,8 +52,7 @@ int main()
 
 """
 
-CPP_TEMPLATE = \
-u"""#include <iostream>
+CPP_TEMPLATE = """#include <iostream>
 #include <string>
 
 int main()
@@ -67,70 +63,115 @@ int main()
 
 """
 
-BASH_TEMPLATE = \
-u"""#!/bin/bash
+BASH_TEMPLATE = """#!/bin/bash
 
 echo template
 """
 
+CMAKE_TEMPLATE = """cmake_minimum_required(VERSION 3.10)
+project({project_name})
+
+add_executable({project_name} {project_name}.cpp)
+
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+"""
+
+
 _EXT_TO_TEMPLATE = {
-    '.py'   : PYTHON_TEMPLATE
-    , '.rb' : RUBY_TEMPLATE
-    , '.c'  : C_TEMPLATE
-
-    , '.cpp': CPP_TEMPLATE
-    , '.cc' : CPP_TEMPLATE
-    , '.cxx': CPP_TEMPLATE
-
-    , '.sh': BASH_TEMPLATE
+    ".py": PYTHON_TEMPLATE,
+    ".rb": RUBY_TEMPLATE,
+    ".c": C_TEMPLATE,
+    ".cpp": CPP_TEMPLATE,
+    ".cc": CPP_TEMPLATE,
+    ".cxx": CPP_TEMPLATE,
+    ".sh": BASH_TEMPLATE,
 }
 
 
 class COLORS:
     """
-        Uses color-set escape sequences:
-        start coloring: \[\e[color\], end coloring: \[\e[m\].
+    Uses color-set escape sequences:
+    start coloring: \[\e[color\], end coloring: \[\e[m\].
     """
-    NOCOLOR = '\033[0m'
-    RED = '\033[1;31m'
-    GREEN = '\033[1;32m'
-    YELLOW = '\033[1;33m'
+
+    NOCOLOR = "\033[0m"
+    RED = "\033[1;31m"
+    GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
 
 
-#TODO: Add simple makefiles for c, cpp templates
-#TODO: print_colored_error() or @colored
+# TODO: print_colored_error() or @colored
+
+
+def exit_if_exists(filename: str):
+    if os.path.exists(filename):
+        print(
+            "%s[!] File %s exists! Specify -f, --force option to replace it!%s"
+            % (COLORS.YELLOW, filename, COLORS.NOCOLOR)
+        )
+        exit(1)
+
+
+def create_cmake_project(project_name: str, force_replace: bool):
+    cmake_filename = "CMakeLists.txt"
+    cpp_filename = project_name + ".cpp"
+    if not force_replace:
+        exit_if_exists(cmake_filename)
+        exit_if_exists(cpp_filename)
+
+    with open(cmake_filename, "wb") as f:
+        template = CMAKE_TEMPLATE.format(project_name=project_name).encode("utf-8")
+        f.write(template)
+
+    with open(cpp_filename, "wb") as f:
+        template = CPP_TEMPLATE.encode("utf-8")
+        f.write(template)
+
 
 def main():
-    DESCR = 'Program creates ready-to-use source file templates for some' \
-            'popular programming languages.'
+    DESCR = (
+        "Program creates ready-to-use source file templates for some"
+        "popular programming languages."
+    )
     parser = argparse.ArgumentParser(description=DESCR)
-    parser.add_argument('filename', type=str, help='filelame of new source file with correct extension')
-    parser.add_argument('-f', '--force', action='store_true',
-                        help='replace existing file')
+    parser.add_argument(
+        "filename", type=str, help="filelame of new source file with correct extension"
+    )
+    parser.add_argument(
+        "-f", "--force", action="store_true", help="replace existing file"
+    )
+    parser.add_argument("--project-name", help="project name for CMakeLists.txt")
     args = parser.parse_args()
     filename = args.filename
+    project_name = args.project_name if args.project_name else "template"
     force_replace = args.force
+
+    if filename == "CMakeLists.txt":
+        create_cmake_project(project_name, force_replace)
+        return
 
     _, ext = os.path.splitext(filename)
     if not ext or ext not in _EXT_TO_TEMPLATE.keys():
-        print("%sError: Wrong extension! Specify one of the supported: %s %s" %
-              (COLORS.RED, _EXT_TO_TEMPLATE.keys(), COLORS.NOCOLOR))
+        print(
+            "%sError: Wrong extension! Specify one of the supported: %s %s"
+            % (COLORS.RED, _EXT_TO_TEMPLATE.keys(), COLORS.NOCOLOR)
+        )
         return
 
-    if os.path.exists(filename) and not force_replace:
-        print("%s[!] File %s exists! Specify -f, --force option to replace it!%s"
-              % (COLORS.YELLOW, filename, COLORS.NOCOLOR))
-        return
+    if not force_replace:
+        exit_if_exists(filename)
 
-    with open(filename, 'wb') as f:
-        f.write(_EXT_TO_TEMPLATE[ext].encode('utf-8'))
+    with open(filename, "wb") as f:
+        template = _EXT_TO_TEMPLATE[ext].encode("utf-8")
+        f.write(template)
 
     # make python sources executable
-    executable_scripts_extentions = ['.py', '.sh', '.rb']
+    executable_scripts_extentions = [".py", ".sh", ".rb"]
     if ext in executable_scripts_extentions:
         st = os.stat(filename)
         os.chmod(filename, st.st_mode | stat.S_IEXEC)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
