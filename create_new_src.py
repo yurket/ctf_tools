f175e8e411101f008f116a1a3dfe353654352ca0
yurket <yurket2006@yandex.ru>
2022-12-03 16:51:01 -0800

    Add a way to quickly add GTest (uncomment the region)

--- create_new_src.py
+++ create_new_src.py
@@ -74,6 +74,16 @@ project({project_name})
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED True)
 
+# include(FetchContent)
+# FetchContent_Declare(
+# googletest
+# GIT_REPOSITORY https://github.com/google/googletest.git
+# GIT_TAG release-1.12.1
+# )
+# # For Windows: Prevent overriding the parent project's compiler/linker settings
+# set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+# FetchContent_MakeAvailable(googletest)
+
 add_executable({project_name} {cpp_filename})
 """
 
